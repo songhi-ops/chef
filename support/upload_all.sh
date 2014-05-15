20c03ee648f1e15b23c0116740254897d6190f0a
Juan Jose Rodriguez Ponce <jjrp78@Juans-MacBook-Air.local>
2014-05-15 12:34:48 +0300

    nginx

---
+++ support/upload_all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+for cookbook in `ls cookbooks | egrep -v README.md` ; do knife cookbook upload $cookbook; done
+for user in `ls data_bags/users.*`; do knife data bag from file users $user; done
+for environment in `ls environments/*.js`; do knife environment from file $environment; done
