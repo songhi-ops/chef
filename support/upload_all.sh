9632e6ff6015774f9c80cb391577a9b348481994
root <root@chef.songhi-ops.com>
2014-05-16 05:06:08 +0000

    Add roles to update_all.sh

--- support/upload_all.sh
+++ support/upload_all.sh
@@ -2,3 +2,4 @@
 for cookbook in `ls cookbooks | egrep -v README.md` ; do knife cookbook upload $cookbook; done
 for user in `ls data_bags/users.*`; do knife data bag from file users $user; done
 for environment in `ls environments/*.js`; do knife environment from file $environment; done
+for role in `ls roles/*.js`; do knife role from file $role; done
