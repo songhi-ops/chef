89b9215cbb281bd128020d34b3db3a58ff6a32a8
Juanjo <juan@hdmessaging.com>
2014-05-16 22:13:14 +0300

    ec2-metadata not in _dev

--- cookbooks/operations/recipes/ec2-metadata.rb
+++ cookbooks/operations/recipes/ec2-metadata.rb
@@ -1,5 +1,5 @@
 
-if node.chef_environment != '_default'
+if node.chef_environment != '_dev'
     remote_file "/usr/bin/ec2-metadata" do
         source "http://s3.amazonaws.com/ec2metadata/ec2-metadata"
         mode 0755
