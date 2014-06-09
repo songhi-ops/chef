3d42936e4b2abe876016945ed454e900f68dd549
Juan Jose Rodriguez Ponce <juan@songhi.com>
2014-06-09 15:59:53 +0300

    Change red color to prompt

--- cookbooks/operations/attributes/default.rb
+++ cookbooks/operations/attributes/default.rb
@@ -5,5 +5,5 @@ default[:operations][:bashrc_file] = '/root/.bashrc'
 default[:operations][:vim][:vim_directory] = '/root/.vim'
 default[:operations][:vim][:vimrc_file] = '/root/.vimrc'
 
-default[:operations][:PS1] = '\[\e[1;31m\][\u@\h \W]\$\[\e[0m\] '
+default[:operations][:PS1] = '\[\e[0;31m\][\u@\h \W]\$\[\e[0m\] '
 
