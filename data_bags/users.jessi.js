d210da48c82f40d74cf049f92b384394d8a5581c
root <root@chef.songhi-ops.com>
2015-02-09 11:32:51 +0000

    svn repos for marketting

---
+++ data_bags/users.jessi.js
@@ -0,0 +1,13 @@
+groups_default:
+  svn_melody_marketing
+  svn_diamond_marketing
+groups_dev:
+  svn_melody_marketing
+  svn_diamond_marketing
+groups_stage:
+  svn_melody_marketing
+  svn_diamond_marketing
+id:             jessi
+password:       $1$jlzQba5n$uIycChTxCKgc4r7Yiw.w0/
+shell:          /bin/bash
+ssh_keys:       ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDA8eNAeLCCYT8XRhbKQTN5P0HWyZWwn/HNMnaBfl25rx/jcO+VRmxoEX63CYTZeIT9DcGVtYNTTzLu1xarUsdHnoZAUXgb8Q9mcMBvX+pLtzJmYuVDNlxq3iRadAbtTtNGF94Wsck8DR5HA6ihx3Q2BN8Bnf7UZNZoFG4kUjFoYkcZb8dmAUbG2LAFbFutmbnS9BAAMX28fVzmwNxmneBoQ7cB0W6XtBjePy627Mf8CFdxUJaUnsscZNUkpLyBo/gLkPxOx+OhYf9D1QnX+jHe19/6uFkAUSzuYV8tYUY2kXM4LvrXqKpApyae5bxTtmddSz1t1GaTEqUIejD7V1oX
