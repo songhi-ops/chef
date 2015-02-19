4843ae2a9e18c00369fa5222cd51004d676bab6c
Juan Jose Rodriguez Ponce <juan@songhi.com>
2015-02-19 16:11:21 +0200

    tomcat

--- cookbooks/tomcat/attributes/default.rb
+++ cookbooks/tomcat/attributes/default.rb
@@ -5,7 +5,7 @@ default[:tomcat][:symlink]="/opt/tomcat"
 default[:tomcat][:log_directory_old]="/opt/apache-tomcat-7.0.53/logs"
 default[:tomcat][:log_directory]="/logs"
 
-if node.chef_environment == "_default"
+if node.chef_environment == "_default" or /_production_/ =~ node.chef_environment
     default[:tomcat][:heap]="-Xms7000m -Xmx7000m"
 else
     default[:tomcat][:heap]="-Xms900m -Xmx900m"
---
+++ data_bags/users.saija.js
@@ -0,0 +1,26 @@
+{
+  "id": "saija",
+  "groups_default": [
+    "svn_melody_marketing",
+    "svn_diamond_marketing",
+    "svn_ruby",
+    "svn_diamond"
+  ],
+  "groups_dev": [
+    "svn_melody_marketing",
+    "svn_diamond_marketing",
+    "svn_ruby",
+    "svn_diamond"
+  ],
+  "groups_stage": [
+    "svn_melody_marketing",
+    "svn_diamond_marketing",
+    "svn_ruby",
+    "svn_diamond"
+  ],
+  "ssh_keys": [
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC1sj5tIeQgJkteEEa+jp5XA+Kakbl1lGHa3b9/wvgQ3RkN9xXSlEEPi8DdVEdxJcChP4spK9eA+KdTJHfvyKtovKi8LVIFqeknnCn25hLuZiHjJEMaokq1dZptQfUJsy2gcS+D8w3juaY9thZ4+P00yLvPLA4s2kStwWvENt2S2AUJGIk1pPjuEZrhRObhs5gBJcPHRX+ntdTbI9Agig1m/BtssBW5fU7fxomFuOvWbWP8DIJSS3tLDxw94m5kYtGNSnGcUn7KOFKQvxfATiKsGqb0ddAOCafa+nwp8Gg+DQ0CnHg8sJkZ/qMlwsCgDVieOb3lgA0Y5kaEDl51ONwv"
+  ],
+  "shell": "/bin/bash",
+  "password": "$1$jlzQba5n$uIycChTxCKgc4r7Yiw.w0/"
+}
