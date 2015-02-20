4fa7c80b6aa32eec1ea642c321aa45204f3a1313
Juan Jose Rodriguez Ponce <juan@songhi.com>
2015-02-20 11:33:43 +0200

    repos

--- data_bags/svn-repos.melody_marketing.js
+++ data_bags/svn-repos.melody_marketing.js
@@ -1,8 +1,8 @@
 {
   "id": "melody_marketing",
-  "users": {
-    "encrypted_data": "1laYfNsBWkHuD4VhLWd+K+LW84+KkmCsBmDlwZATZUJ8YsadlORx1JLMuWUA\nKug29Vd+h7EwoC8wEHMtIWterAAa7sSxb81DyZoyTMJArlM=\n",
-    "iv": "ngC6F+NY8BzBTOj/sOhywA==\n",
+  "groups": {
+    "encrypted_data": "3o53cDVEHdcF+ZRItHXSwoElvcD60YJDzcLwrEoDPovdwgjXD/03RdN4ZqMo\nMeuAR+wetYbyDo2MmqC3QZWgXy4za0xrGiORPkd9OwqOoJwevOQZDflns2Gw\nwVdsaZOyktWgOu7aAfnr/KqgGUxDDKiWpS/ereKhX3CNrMldj0QkrK2Xi6wL\nc6bHY8AvV4Yl/s7nUNmss6Sc+eF4hzI34J4d6hgZh0olry/JmGZn+pwcnsxH\nCINjQBi8GZYsswJ9\n",
+    "iv": "QSFC76uKiW8QzYnbS+fg8A==\n",
     "version": 1,
     "cipher": "aes-256-cbc"
   }
---
+++ data_bags/svn-repos.songhi_branding.js
@@ -0,0 +1,9 @@
+{
+  "id": "songhi_branding",
+  "groups": {
+    "encrypted_data": "9u90S7pNEnubMgn1B1Ff15ixnrRZnNjBtzvWFG/eue8cpjZOF75pcEwTqoQ+\n07A3WpHIuA0G7Yc+Nidxg2HZv2LJ3EW4d25BqvOrK64X0IcxgaYs25ujUPlf\n+g+KDeEnq+x+eu/oWOsjf6KunXACXw==\n",
+    "iv": "P16HKFUrlCfZ8DSTpSHZnw==\n",
+    "version": 1,
+    "cipher": "aes-256-cbc"
+  }
+}
