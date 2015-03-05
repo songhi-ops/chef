f246b05ba6b49768ff81326cfa3edc2edee79dfb
root <root@chef.songhi-ops.com>
2015-03-05 13:15:28 +0000

    Created new external access for diamond_marketing to Bidalgo, for Jessi

---
+++ data_bags/svn_repos.diamond_marketing.js
@@ -0,0 +1,9 @@
+{
+  "id": "diamond_marketing",
+  "groups": {
+    "encrypted_data": "REiC1nP2RMjSBSQIKfpL4gZyZbqbKOmlcjB8vVTq3m6JJ6EESrZf8cau9kOv\nnC+HHFBnaYdCT6DMIkz6cW44UDrPUz4Cm5ptsx6t+PlHsYHyvTV4d7Ywnq41\n/Qvap0bqEF+u1A4HlCnLSvMen732iXtUufuD3WuPUHtvKakVwjUYS95dSqeY\nY1KL+bUiyz2IrBKVNp/Lmat5esEqxQtmDwkNt0lXQ9BPA65oRD5W+pqx3cUh\nmuTbZ9wAlOxwIKybTRDpcVXjkI6+ftSeWIL96Q==\n",
+    "iv": "EDQswKpYb3lXGWblHdG2rQ==\n",
+    "version": 1,
+    "cipher": "aes-256-cbc"
+  }
+}
