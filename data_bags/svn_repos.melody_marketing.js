1b62001d413e9d7f6a518bd916d4fa5efb2db8a7
root <root@chef.songhi-ops.com>
2015-03-08 20:59:42 +0000

    Unify bidalgo password

--- data_bags/svn_repos.melody_marketing.js
+++ data_bags/svn_repos.melody_marketing.js
@@ -1,9 +1,14 @@
-{
-  "id": "melody_marketing",
-  "groups": {
-    "encrypted_data": "jgoXWHtPHXtNei3s+DTETRsMdHrp91b6vvfTLVzJdy30wCNL6XilcxDX74Ch\nyJpWg1vmsbF+Tt1exOL7vuRgUCtX4LiUdJnFfyWxg46G8lT7xKGYI/L90BhM\nMzLxWm3pFtn7JCzgnGEV4rnM3jr0J4eA0uXBzQpbSsYx+Ruh1QL/NMenlCT9\nAxeEriSVjasY4W+dmM9R2KFIqhhnmCSmFcaAnToyHvB7ioIUuodJjufpgirg\n9zJS9wxpYs0llbIet4BgN165EocabT27yhUcMMfx+OE6srZnl8cg9CkH6UTu\nG+O3jhSPa7j96ZNt27FOtSZlrpQ8ZbHjULKQokFN1160E/RuK3LOZreF0JYy\n1wOBPK3YV/WfT45hfFcwlyix\n",
-    "iv": "JNjsHamgh4Z4Io02vDvhiQ==\n",
-    "version": 1,
-    "cipher": "aes-256-cbc"
-  }
-}
+groups:
+  cipher:         aes-256-cbc
+  encrypted_data: EHmokQZ626TMc12g/chdNQI/sKK5vJvLs0guGAKkDKSSG7hksPTZGQNfaW+x
+  6Ur8RVi9otudLE5OL7/btY8y8PjvnbpWYSEIdxrw6KRqi7hxtifakPxZYFUy
+  V1IYhiE8e4KneFyTllMirTZWrFIzA+RZExhyAjm0LTSTB3n1ld8QlSi8ubJi
+  Yh1wZLWaWKPh1MsLGsb+7qiUSoeFi3wTW249LyH5PSdGSQfJu6SfOL1vedWC
+  14aoLk1AoipfwhLZDZGQO/cVLbyOAzKi+fhQdDpHaKE+yQ4OevoX0+QPTu1j
+  WlcNsEThRQ0p6y7lK3qsUeSSliMYP7ipwpjBxHm1/UbuXo+jEiBHFBnvT4yK
+  v6VnYXgDxpecHl6862YK1apE
+  
+  iv:             XHUVrZ0kk49oCqsOVNWBUQ==
+  
+  version:        1
+id:     melody_marketing
---
+++ data_bags/svn_repos.melodymonsters_branding.js
@@ -0,0 +1,10 @@
+groups:
+  cipher:         aes-256-cbc
+  encrypted_data: N3P75elkS4QhYRBupIlYyhyBPbEjfIJr7rv1mO6x+3GVIuGp+ACUYYsaTg8u
+  8cab31trNtk3yNuGI2WLnXruylSZQMfjhJwsvLAYmrQaUNO/fsIhX8qlUdO1
+  scd6ygWsxrirPt7yW8353H4G5CmRNA==
+  
+  iv:             AE4OLigfrvlvV9KsXki6Zg==
+  
+  version:        1
+id:     melodymonsters_branding
