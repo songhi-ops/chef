90effec105863aa2485d4abbc7e001cb98b75cb1
root <root@chef.songhi-ops.com>
2015-04-16 08:34:05 +0000

    Created user for Maxime and gave him access to repositories.

---
+++ data_bags/users.maxime.js
@@ -0,0 +1,17 @@
+{
+  "id": "maxime",
+  "groups_default": [
+    "svn_gametroopers_melody"
+  ],
+  "groups_dev": [
+    "svn_gametroopers_melody"
+  ],
+  "groups_stage": [
+    "svn_gametroopers_melody"
+  ],
+  "ssh_keys": [
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDUsiEAuEEoMihgj48kWYx3JY5Lf0Kl97bnHM9HTDwI1vj592dPiKwKl7sZqQtIVfBaAF6IYqCP9Kn2wRziGk0JkLI72G5iMd5kb/PFui8JgFdOBrLU4Ce4GTD2mdt8UFpPJWFi3DMS3EjdFfK8M0biW6i/NxctoirUQK34rnBc5KzxWSo518mqV6Nf7DcZaV8epmcXFdsF5CueHJB1Xh2C0p+mSsyr11NrVGpDv1i+38VuvX5FTOuCVI6AsOWaMLZxwvOA9K3TkxN+1lPn96kl0ct5gDLXqtSP8FUja0YqcsgUPEKSXmNAJZp9ZYNVeu2nrDlL3KQ8D3/PW8lAcpN9 maxime@songhi.com"
+  ],
+  "shell": "/bin/bash",
+  "password": "$1$H06GY1.L$xp6C4xADrYhCgBqhh9a/O1"
+}
