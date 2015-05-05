b229bc210d1aa0203c9189235516366e82eb9f4c
root <root@chef.songhi-ops.com>
2015-05-05 09:29:05 +0000

    remove petri, open redis port stage

--- roles/magic-stage.js
+++ roles/magic-stage.js
@@ -37,6 +37,9 @@
             "900-ntp": {
               "rule": "--protocol udp --dport 123 --jump ACCEPT"
             },
+            "1000-redis": {
+              "rule": "--protocol udp --dport 6379 --jump ACCEPT"
+            },
             "default": "DROP [0:0]"
           }
         }
