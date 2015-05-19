621580b767efbe41fb6a83aba6d58669a35d46f6
root <root@chef.songhi-ops.com>
2015-05-19 12:53:45 +0000

    New access for Jessi. Bidalgo for Melody Monsters

--- data_bags/svn_repos.melody_marketing.js
+++ data_bags/svn_repos.melody_marketing.js
@@ -1,8 +1,8 @@
 {
   "id": "melody_marketing",
   "groups": {
-    "encrypted_data": "n7oBMrN9im09EfyUKkr2YEfRVa8TbnKgqu7fo2GkdeYy4eTeOttbifB2r171\nrWEtVT4pucWgwSvyR6ZCRxETiIybjqLefY/5NyRy40l8fQ7Ol9Ep0inFoEN+\nig+smz2YMZTP3wfSTc0P39n4ki6G1AwBB4YI8MnqWMbczWxj5n2Rqq4DnRKR\n1eVj0S0B4FkShelW/73Ja6LMSmULCCtYvbfBcxDbH+Ib2WS0HiKlz9IIVSpJ\nqq9UGtfJEWUJTboA5G4qmJIEdORwPaR0oXP70nHbO9UR8Knra5MkgcQEshxj\nKmU54tmIRqFgBQV99L148i5oZcUPCS5QpukSpMzMpI/GhqmPFb2RjmtefZ1u\nduxYBFLTvf3x9j6aJo74WhSWf2Xwp9YpcPYBNLFXvEaaUTCGzhxVPZqq5xpk\nlpA+xeyDldEqVpOis9c2XGn1aRgCH6quEMEONB1pN+Afe6STVtJ7ZVudVA0z\nG0H2l9do8Gs=\n",
-    "iv": "/GNm2Yixv89hD+2qhJcAYA==\n",
+    "encrypted_data": "cgvPn0fW81OAzKYb9SWyscB8vDILNfHvZoJ90V/JMkXSCx3hEetT8UMtdTLl\nTZFKp3/QfKHLoqYW/usN9gSug9Ozny7wucDp+y+4aFkXW4sXOTGmRbIwpK0J\nL0TFxx7vNb6wFo3dfLe0d1pSoQVsEnnE47oe+Gs8T4f+ULSpyL3mBLWdEJev\nSdrjXo6t5+AG1/7BKJDWlL5Z40oml3FM2RhOGSbAfc9m7ZW6L0z7C9piK0OX\n2rsRKAuI8t0840JBuufya0fR1WrNnuJYbYzaCGxL/dyxypZ8nE2sojYo7S0I\nkb0T0xKut6U+5fItSkicrWppf7afcDRm1uAbmnS4qqeBCv/TDoa29B7W9mAn\nyAwp1yaJbGVV8lhdsQt+LEy7NGcmMq7q/v3fCuaSWyMVp0P7KLp/s44DDGxj\nrYgIxJnbAfRjPS2dXarURFu0D8ijBAmEu26oCajA//M9uTg4oLhYwcqNShQS\n4igXvPiCbpJlJeMKmX2tN5/80gjq9fEvi6xl5O+xiw/TSBbE60vdeRWoT9t0\nuhRj8y0omoyxOmJWQTxW911gAUl4g/+xMTY81Fw2UmwoPAzWrNWfcWwH9g==\n",
+    "iv": "ENMyIf6fax8leBiIYtMfqQ==\n",
     "version": 1,
     "cipher": "aes-256-cbc"
   }
